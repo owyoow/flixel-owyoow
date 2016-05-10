6e20a06915d6ba38fd09335dcb1475ecaada9ba2
Jurien Meerlo <jurienmeerlo@live.nl>
2016-05-10 23:02:44 +0100

    added separate int clamp

--- owyoow/math/MathUtils.hx
+++ owyoow/math/MathUtils.hx
@@ -3,7 +3,23 @@ package owyoow.math;
 class MathUtils
 {
     // static function to clamp a float value to a min and a max
-    public static function Clamp (value:Float, min:Float, max:Float):Float
+    public static function clampF (value:Float, min:Float, max:Float):Float
+    {
+        if (value < min)
+        {
+            return min;
+        }
+        else if (value > max)
+        {
+            return max;
+        }
+        else
+        {
+            return value;
+        }
+    }
+
+    public static function clamp (value:Int, min:Int, max:Int):Int
     {
         if (value < min)
         {
