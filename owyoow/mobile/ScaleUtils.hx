6ba191a4c92005399aa4e2bbe89620068dbfd9ec
Jurien Meerlo <jurienmeerlo@live.nl>
2016-06-05 12:28:09 +0100

    added a few comments to make it more clear what this class is for

--- owyoow/mobile/ScaleUtils.hx
+++ owyoow/mobile/ScaleUtils.hx
@@ -1,21 +1,28 @@
 package owyoow.mobile;
 
+// this class should be used together with MobileScaleIos.hx 
+
+// this is a simple class with a few functions to make using assets of multiple sizes easier
 class ScaleUtils
 {
     private static var scale:Float;
     private static var suffix:String;
 
+    // set the scale and suffix for use in the functions below
+    // this should not be used as a static extesion method
     public static function setScale (mobileScale:Float, mobileSuffix:String):Void
     {
         scale = mobileScale;
         suffix = mobileSuffix;
     }
 
+    // just multiply the value with the scale to get the correct value depending on the device size
     public static function toScale (value:Float):Float
     {
         return value * scale;
     }
 
+    // add the @2x or @4x type of suffix to the end of the path name to load the correct assets
     public static function addSuffix (path:String):String
     {
         var index:Int = path.lastIndexOf(".");
